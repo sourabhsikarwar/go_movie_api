a898f5f5e4561eba6a45daa0303f64dc42593391
Sourabh Sikarwar <sourabhsikarwar1812@gmail.com>
2024-10-02 23:33:27 +0530

    Controllers

--- controllers/movies.go
+++ controllers/movies.go
@@ -1,123 +1,84 @@
 package controllers
 
 import (
-	"context"
-	"fmt"
-	"log"
+	"encoding/json"
+	"net/http"
 
-	"github.com/sourabhsikarwar/go_movie_api/db"
+	"github.com/gorilla/mux"
+	"github.com/sourabhsikarwar/go_movie_api/helpers"
 	"github.com/sourabhsikarwar/go_movie_api/models"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// ------------- Modifications Endpoints --------------------
-func insertMovie(movie models.Movies) {
-	if movie.Movie == "" {
-		fmt.Println("No movie title provided")
+func GetAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	allMovies := helpers.GetAllMovies()
+	json.NewEncoder(w).Encode(allMovies)
+}
+
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	var movie models.Movies
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	res, err := helpers.InsertMovie(movie)
+
+	jsonEncoder := json.NewEncoder(w)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		jsonEncoder.Encode(map[string]string{"error": err.Error()})
 		return
 	}
-	inserted, err := db.Collection.InsertOne(context.Background(), movie)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Movie inserted with ID:", inserted.InsertedID)
+	w.WriteHeader(http.StatusCreated)
+
+	// using interface for different type of datatypes
+	jsonEncoder.Encode(map[string]interface{}{
+		"message": res,
+		"movie":   movie,
+	})
 }
 
-func updateMovie(movieId string) {
-	if movieId == "" {
-		fmt.Println("No movie ID provided")
-		return
-	}
-	id, err := primitive.ObjectIDFromHex(movieId)
-	if err != nil {
-		log.Fatal(err)
-	}
+func MarkAsWatched(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "PUT")
 
-	filter := bson.M{"_id": id}
-	update := bson.M{"$set": bson.M{"watched": true}}
+	params := mux.Vars(r)
+	movieId := params["id"]
 
-	res, err := db.Collection.UpdateOne(context.Background(), filter, update)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Movie updated", res.ModifiedCount)
+	helpers.UpdateMovie(movieId)
+
+	w.WriteHeader(http.StatusAccepted)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message": "Movie updated Successfully",
+		"id":      movieId,
+	})
 }
 
-func deleteMovie(movieId string) {
-	if movieId == "" {
-		fmt.Println("No movie ID provided")
-		return
-	}
-	id, err := primitive.ObjectIDFromHex(movieId)
-	if err != nil {
-		log.Fatal(err)
-	}
+func DeleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
 
-	filter := bson.M{"_id": id}
-	res, err := db.Collection.DeleteOne(context.Background(), filter)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Movie deleted", res.DeletedCount)
+	params := mux.Vars(r)
+	movieId := params["id"]
+
+	helpers.DeleteMovie(movieId)
+
+	w.WriteHeader(http.StatusAccepted)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message": "Movie deleted Successfully",
+		"id":      movieId,
+	})
 }
 
-func deleteMovies(movieIds []string) {
-	if len(movieIds) == 0 {
-		fmt.Println("No movies to delete")
-		return
-	}
-	var idList []primitive.ObjectID
+func DeleteAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "DELETE")
 
-	for _, id := range movieIds {
-		oid, err := primitive.ObjectIDFromHex(id)
-		if err != nil {
-			log.Fatal(err)
-		}
-		idList = append(idList, oid)
-	}
+	helpers.DeleteAllMovies()
 
-	filter := bson.M{"_id": bson.M{"$in": idList}}
-	res, err := db.Collection.DeleteMany(context.Background(), filter)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Movies deleted", res.DeletedCount)
-}
-
-func deleteAllMovies() {
-	res, err := db.Collection.DeleteMany(context.Background(), bson.D{{}}, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("All Movies deleted", res.DeletedCount)
-}
-
-// ----------------------------------
-
-// ----------------- Reading Endpoints ---------------
-
-func getAllMovies() []primitive.M {
-	// Here mongo db returns a cursor object from which we can loop and get individual object or data
-	cursor, err := db.Collection.Find(context.Background(), bson.M{})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	var movies []primitive.M
-
-	// Looping until the curson.Next has a valid value
-	// This works like a while loop
-	for cursor.Next(context.Background()) {
-		var movie bson.M
-
-		// passing reference of movie variable to store the error value for checking
-		if err := cursor.Decode(&movie); err != nil {
-			log.Fatal(err)
-		}
-		movies = append(movies, movie)
-	}
-	// closing the context for the cursor
-	defer cursor.Close(context.Background())
-	return movies
+	w.WriteHeader(http.StatusAccepted)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message": "All Movies deleted Successfully",
+	})
 }
--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,1 +1,126 @@
 package helpers
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/sourabhsikarwar/go_movie_api/db"
+	"github.com/sourabhsikarwar/go_movie_api/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// This file contains all the Database controllers
+// These are useful for the controllers functions
+
+// ------------- Modifications Endpoints --------------------
+func InsertMovie(movie models.Movies) (string, error) {
+	if movie.Movie == "" {
+		return "", fmt.Errorf("no movie title provided")
+	}
+	inserted, err := db.Collection.InsertOne(context.Background(), movie)
+	if err != nil {
+		return "", err
+	}
+	message := fmt.Sprintf("Movie inserted with ID: %v", inserted.InsertedID)
+	return message, nil
+}
+
+func UpdateMovie(movieId string) {
+	if movieId == "" {
+		fmt.Println("No movie ID provided")
+		return
+	}
+	id, err := primitive.ObjectIDFromHex(movieId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	res, err := db.Collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Movie updated", res.ModifiedCount)
+}
+
+func DeleteMovie(movieId string) {
+	if movieId == "" {
+		fmt.Println("No movie ID provided")
+		return
+	}
+	id, err := primitive.ObjectIDFromHex(movieId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := bson.M{"_id": id}
+	res, err := db.Collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Movie deleted", res.DeletedCount)
+}
+
+func DeleteMovies(movieIds []string) {
+	if len(movieIds) == 0 {
+		fmt.Println("No movies to delete")
+		return
+	}
+	var idList []primitive.ObjectID
+
+	for _, id := range movieIds {
+		oid, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		idList = append(idList, oid)
+	}
+
+	filter := bson.M{"_id": bson.M{"$in": idList}}
+	res, err := db.Collection.DeleteMany(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Movies deleted", res.DeletedCount)
+}
+
+func DeleteAllMovies() {
+	res, err := db.Collection.DeleteMany(context.Background(), bson.D{{}}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("All Movies deleted", res.DeletedCount)
+}
+
+// ----------------------------------
+
+// ----------------- Reading Endpoints ---------------
+
+func GetAllMovies() []primitive.M {
+	// Here mongo db returns a cursor object from which we can loop and get individual object or data
+	cursor, err := db.Collection.Find(context.Background(), bson.M{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	var movies []primitive.M
+
+	// Looping until the curson.Next has a valid value
+	// This works like a while loop
+	for cursor.Next(context.Background()) {
+		var movie bson.M
+
+		// passing reference of movie variable to store the error value for checking
+		if err := cursor.Decode(&movie); err != nil {
+			log.Fatal(err)
+		}
+		movies = append(movies, movie)
+	}
+	// closing the context for the cursor
+	defer cursor.Close(context.Background())
+	return movies
+}
